4df3ea0dbc667b52d28a5b46e721c6e0369ce2e0
Mira Shanouda <Mira.shanouda@aucegypt.edu>
2022-01-02 01:06:22 +0200

    Finally solved: I forgot creating a new node at the begining 'head = new ListNode()'

--- LC_LL_sorting.cpp
+++ LC_LL_sorting.cpp
@@ -21,30 +21,85 @@ using namespace std;
 };
 
 ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    //base case 
-    //if (l1 == NULL) return l2;
-    //if (l2 == NULL) return l1;
-//
-    ////using dummy node: 
-    //ListNode * dummy, * head; 
-    //dummy = head; 
-//
-    //while (l1 != NULL && l2 != NULL){
-    //    if(l1->val <= l2->val){
-    //        dummy->next = l1;
-    //        l1 = l1->next;
-    //    }
-    //    else {
-    //        dummy->next = l2;
-    //        l2 = l2->next;
-    //    }
-    //    dummy = dummy->next;
-    //}
-    //
-    //if(l1 != NULL)
-    //    dummy->next = l1; 
-    //else
-    //    dummy->next = l2; 
-//
-    //return head->next;
+
+    //recursion: 
+        //base case 
+        //if (l1 == NULL) return l2;
+        //if (l2 == NULL) return l1;
+        //
+        //if (l1->val < l2->val) {
+        //    l1->next = mergeTwoLists(l1->next, l2);
+        //    return l1;
+        //}
+        //else {
+        //    l2->next = mergeTwoLists(l1, l2->next);
+        //    return l2;
+        //}
+        
+    //using dummy node: 
+    ListNode * dummy, * head;
+    head = new ListNode(); //this is important; without it I am not creating a new list 
+    dummy = head; 
+
+    while (l1 != NULL && l2 != NULL){
+        if(l1->val <= l2->val){
+            //cout << "I entered l1\n"; 
+            dummy->next = l1;
+            l1 = l1->next;
+        }
+        else {
+            //cout << "I entered l2\n"; 
+            dummy->next = l2;
+            l2 = l2->next;
+        }
+        //cout << "Dummy:" << dummy->val << endl; 
+        
+        dummy = dummy->next;
+        
+    }
+
+
+    //cout << "Head: " << head->next->val << endl;
+    if(l1 != NULL)
+        dummy->next = l1; 
+    else
+        dummy->next = l2; 
+
+    return head->next;
+}
+
+int main(){
+
+    ListNode *l1, *l2;
+
+    l1 = new ListNode();
+    l2 = new ListNode();
+
+    //l1 = [1,2,4], l2 = [1,3,4]
+    ListNode *l11 = new ListNode(), *l111 = new ListNode(); 
+    l1->val = 1;
+    l1->next = l11;
+
+    l11->val = 2;
+    l11->next = l111;
+
+    l111->val = 4;
+    l111->next = NULL;
+//*********************************************************
+    ListNode *l22 = new ListNode(), *l222 = new ListNode(); 
+    l2->val = 1;
+    l2->next = l22;
+
+    l22->val = 3;
+    l22->next = l222;
+
+    l222->val = 4;
+    l222->next = NULL;
+
+    l1 = mergeTwoLists(l1, l2);
+    while(l1 != NULL){
+        cout << l1->val << endl;
+        l1= l1->next;
+    }
+    return 0;
 }
