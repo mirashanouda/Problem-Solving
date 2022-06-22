e95726331944a76813b31ccd34d89d788ceea78d
Mira Shanouda <67865802+mirashanouda@users.noreply.github.com>
2022-06-23 01:33:00 +0200

    list with size, For Loop starting from 1

---
+++ LC_runningSum.py
@@ -0,0 +1,13 @@
+"""
+Outcomes: 
+- Decaring a list with a specific size
+- For Loop starting from 1
+- return a list
+"""
+def runningSum(self, nums: List[int]) -> List[int]:
+    new_list = [None] * len(nums)
+    new_list[0] = nums[0]
+    i = 1
+    for i in range(1,len(nums)):
+        new_list[i] = new_list[i-1] + nums[i]
+    return new_list
